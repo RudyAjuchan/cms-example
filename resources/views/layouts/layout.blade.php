842cd7cad3a45bdd5e8f3000181d11b3f93bd13a
Rudy Ajuchán <rudyajuchansec32@gmail.com>
2023-07-24 15:39:25 -0600

    estilo agregado

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="icon" href="assets/images/favicon.ico">
-    <link href="assets/css/style.css" rel="stylesheet">
+    {{-- <link href="assets/css/style.css" rel="stylesheet"> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css" />
     <link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.4.1/css/rowReorder.dataTables.min.css">
@@ -16,8 +16,8 @@
     <link type="text/css" href="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/css/dataTables.checkboxes.css" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/table.css">
     <!-- Scripts -->
-    @vite(['resources/css/app.css'])
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    {{-- @vite(['resources/css/app.css']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body x-data="{ page: 'ecommerce', 'loaded': true, 'darkMode': true, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }" x-init="
@@ -178,7 +178,7 @@
                         <div class="relative" x-data="{ dropdownOpen: false }" @click.outside="dropdownOpen = false">
                             <a class="flex items-center gap-4" href="#" @click.prevent="dropdownOpen = ! dropdownOpen">
                                 <span class="hidden text-right lg:block">
-                                    <span class="block text-sm text-black font-medium">{{ Auth::user()->name }}</span>
+                                    <span class="block text-sm text-black font-medium dark:text-white">{{ Auth::user()->name }}</span>
                                     <span class="block text-xs font-medium">Administrador</span>
                                 </span>
 
@@ -237,7 +237,7 @@
     <script src="https://cdn.datatables.net/rowreorder/1.4.1/js/dataTables.rowReorder.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
     <script type="text/javascript" src="//gyrocode.github.io/jquery-datatables-checkboxes/1.2.12/js/dataTables.checkboxes.min.js"></script>
-    <script defer src="assets/js/bundle.js"></script>
+    {{-- <script defer src="assets/js/bundle.js"></script> --}}
     <script src="assets/js/table.js"></script>
 </body>
 
